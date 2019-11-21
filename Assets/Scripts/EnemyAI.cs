aee6b1164eccf010d853a35e68105f045bdbf26a
YacineL <yacine9258@gmail.com>
2019-11-21 23:22:05 +0100

    [EVO] : adds an enemy AI script

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
