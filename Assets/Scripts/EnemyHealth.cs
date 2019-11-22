ae5a8434c43e4ed4745f41f49f853109ee365840
YacineL <yacine9258@gmail.com>
2019-11-22 15:03:33 +0100

    [EVO] : adds a shooting function and VFX

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float healthPoints = 100f;
+    public void TakeDamage(float damage)
+    {
+        healthPoints -= damage;
+        if (healthPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,11 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera fPCamera;
+    [SerializeField] float weaponDamage = 30f;
     [SerializeField] float rangeOfShot = 100f;
+    [SerializeField] UnityEvent onShoot;
+
     void Update()
     {
         if(Input.GetButtonDown("Fire1"))
@@ -17,8 +21,25 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        onShoot.Invoke();
+        ProcessRaycast();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        Physics.Raycast(fPCamera.transform.position, fPCamera.transform.forward, out hit, rangeOfShot);
-        print(hit.transform.name);
+        if (Physics.Raycast(fPCamera.transform.position, fPCamera.transform.forward, out hit, rangeOfShot))
+        {
+            print(hit.transform.name);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target != null)
+            {
+                target.TakeDamage(weaponDamage);
+            }
+        }
+        else
+        {
+            return;
+        }
     }
 }
