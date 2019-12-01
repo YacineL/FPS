a821d71234d11f8668795447cc4cd927df9a276c
YacineL <yacine9258@gmail.com>
2019-12-01 03:25:07 +0100

    [EVO] : adds a flashlight system and a battery pickup

---
+++ Assets/Scripts/BatteryPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BatteryPickup : MonoBehaviour
+{
+    [SerializeField] float restoreAngle = 90f;
+    [SerializeField] float addIntensity = 1f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject weapon = GameObject.FindGameObjectWithTag("Weapon");
+            weapon.GetComponentInChildren<FlashlightSystem>().RestoreLightAngle(restoreAngle);
+            weapon.GetComponentInChildren<FlashlightSystem>().AddLightIntensity(addIntensity);
+            Destroy(gameObject);
+        }
+    }
+
+}
